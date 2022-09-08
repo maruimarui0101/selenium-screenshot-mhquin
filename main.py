d5ae4d908a14e97b4d57659fe41a3d61c8ca2a51
maruimarui0101 <22180616+maruimarui0101@users.noreply.github.com>
2022-09-08 01:26:12 +0100

    Added capability to click on accept cookies button on amazon.

--- main.py
+++ main.py
@@ -5,23 +5,34 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument('disable-notifications')
+chrome_options.add_argument("user-agent=UA")
 
-urls_list = ['https://yahoo.com/', 'https://eff.org/', 'https://invidious.io/']
+urls_list = ['https://amazon.co.uk']
 
 def get_secondly_screenshots(list_of_urls):
-    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     for url in list_of_urls:
-        driver.get(url)
-        now = datetime.now()
-        date_time = now.strftime("%Y_%m_%d_%H_%M_%S")
-        sh_url = url.split('://')[1].split('.')[0]
-        print(sh_url, date_time)
-        driver.save_screenshot(f'{sh_url}_{date_time}.png')
+        # driver.get(url)
+        # now = datetime.now()
+        # date_time = now.strftime("%Y_%m_%d_%H_%M_%S")
+        # sh_url = url.split('://')[1].split('.')[0]
+        # print(sh_url, date_time)
+        # driver.save_screenshot(f'{sh_url}_{date_time}.png')
 
-        print('screenshotted ', url)
+        # print('screenshotted ', url)
+
+        driver.get(url)
+        test = driver.find_element('xpath', '//*[@id="sp-cc-accept"]')
+        test.click()
+
         t.sleep(2)
     driver.quit()
 
